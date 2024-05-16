280185c018a0a653f456960374e27048e09fffc4
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-16 15:49:01 +0300

    .

--- reducer.js
+++ reducer.js
@@ -1,10 +1,14 @@
 const initialState = [];
+const action = {
+  type: "ADD_NAME",
+  payload: "Alice",
+};
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'ADD_TODO':
-          return [...state, action.payload];
-      case 'DELETE_TODO':
-            return state.filter((todo) => todo.id !== action.payload);
+    case "ADD_NAME":
+      return [...state, action.payload];
+    case "REMOVE_NAME":
+      return state.filter((name) => name !== action.payload);
     default:
       return state;
   }
