2948e8443fb452f0b76c66152444b5fb40d48ba2
Sveta Lantsman <malishkin2002@gmail.com>
2024-05-16 16:31:51 +0300

    .

--- reducer.js
+++ reducer.js
@@ -6,6 +6,8 @@ function reducer(state, action) {
       return [...state, action.payload];
     case "REMOVE_NAME":
       return state.filter((name) => name !== action.payload);
+    case "CLEAR_ALL":
+      return [];
     default:
       return state;
   }
@@ -16,5 +18,26 @@ console.log(newState); // ["Bogdan"]
 newState = reducer(newState, { type: "ADD_NAME", payload: "Alice" });
 console.log(newState);
 
-let stateDeleted = reducer(newState, { type: "REMOVE_NAME", payload: "Bogdan" });
+let stateDeleted = reducer(newState, {
+  type: "REMOVE_NAME",
+  payload: "Bogdan",
+});
 console.log(stateDeleted); // ["Alice"]
+
+newState = reducer(newState, { type: "ADD_NAME", payload: "Bogdan" });
+console.log(newState);
+
+newState = reducer(stateDeleted, { type: "ADD_NAME", payload: "Bogdan" });
+console.log(newState);
+
+newState = reducer(newState, { type: "CLEAR_ALL" });
+console.log(newState); // []
+newState = reducer(stateDeleted, { type: "ADD_NAME", payload: "Bogdan" });
+console.log(newState);
+newState = reducer(newState, { type: "ADD_NAME", payload: "Bogdan" });
+console.log(newState);
+let cleardState = reducer(newState, { type: "CLEAR_ALL" });
+console.log(cleardState); // []
+cleardState = reducer(cleardState, { type: "ADD_NAME", payload: "Bogdan" });
+console.log(cleardState);
+
